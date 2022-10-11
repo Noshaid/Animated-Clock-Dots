407591e9a127cc8731fdebe327d560f1a3142ae3
Noshaid Ali <noshaid.ali@coeus-solutions.de>
2022-10-11 18:26:13 +0500

    code pushed

---
+++ components/Square.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useDeferredValue } from 'react'
+import { N, SQUARE_SIZE } from '../constants'
+import Animated, { useAnimatedStyle, useDerivedValue, withSpring, withTiming } from 'react-native-reanimated';
+
+interface SquareProps {
+    index: number;
+    progress: Animated.SharedValue<number>;
+}
+
+const Square: React.FC<SquareProps> = ({ index, progress }) => {
+
+    const offestAngle = (2 * Math.PI) / N;
+    const finalAngle = offestAngle * (N - 1 - index);
+
+    const rotate = useDerivedValue(() => {
+        if(progress.value <= 2 * Math.PI) {
+            return Math.min(finalAngle, progress.value);
+        }
+        if(progress.value - 2 * Math.PI < finalAngle) {
+            return finalAngle;
+        }
+        return progress.value
+    }, []);
+
+    const translateY = useDerivedValue(() => {
+        if(rotate.value === finalAngle) {
+            return withSpring(-N * SQUARE_SIZE)
+        }
+        if(progress.value > 2 * Math.PI) {
+            return withTiming((index - N) * SQUARE_SIZE);
+        }
+        return withTiming(-index * SQUARE_SIZE);
+    }, [])
+    
+    const reAnimatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {rotate: `${rotate.value}rad`},
+                {translateY: translateY.value},
+            ]
+        }
+    });
+    
+  return (
+    <Animated.View 
+    style={[{
+        height: SQUARE_SIZE,
+        aspectRatio: 1,
+        backgroundColor: 'white',
+        opacity: (index+1)/N,
+        position: 'absolute',
+      }, reAnimatedStyle]} />
+  )
+}
+
+export default Square;
+
+const styles = StyleSheet.create({})
